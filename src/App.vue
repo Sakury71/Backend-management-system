e4b2bafb251b4f57eded66ad99fd1e456d0f27d1
Sakury <206842393@qq.com>
2024-05-30 19:24:08 +0800

    测试提交

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="box">
+    <h1>你好,Vue3！</h1>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  width: 600px;
+  height: 400px;
+
+  h1 {
+    color: black;
+  }
+}
+</style>
